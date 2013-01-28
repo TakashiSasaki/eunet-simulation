16944d34d0109910af3494921ed1ba0460bf0bed
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2013-01-28 17:00:42 +0900

    Comment lines for csmaSwitches was moved to corresponding header file.

---
+++ Terminals.h
@@ -0,0 +1,17 @@
+/*
+ * Terminals.h
+ *
+ *  Created on: Jan 28, 2013
+ *      Author: sasaki
+ */
+
+#ifndef TERMINALS_H_
+#define TERMINALS_H_
+
+class Terminals {
+public:
+	Terminals();
+	virtual ~Terminals();
+};
+
+#endif /* TERMINALS_H_ */
