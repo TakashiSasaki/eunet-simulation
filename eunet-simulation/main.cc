be1916639e36bf44e5a3e46528732deb3bf545c2
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2013-02-03 00:52:06 +0900

    some commented out lines were removed

--- eunet-simulation/main.cc
+++ eunet-simulation/main.cc
@@ -39,52 +39,12 @@ int main(int argc, char** argv) {
 	cmd.Parse(argc, argv);
 
 	Eunet eunet;
+
 	TerminalSets terminal_sets(350);
 	eunet.Attach(terminal_sets);
 
-//	for (int i = 0; i < 350; ++i) {
-//		terminal_sets.push_back(new TerminalSet);
-//	} //for
-//	for (int j = 5; j <= 348; ++j) {
-//		if (j == 80 || j == 113 || j == 160 || j == 213 || j == 271 || j == 333)
-//			continue;
-//		eunet.Attach(*terminal_sets[j], j);
-//		SimpleNode & csma_switch = *csmaSwitches[j];
-//		TerminalSet & terminal_set = *terminal_sets[j];
-//		for (int i = 0; i < 15; ++i) {
-//			SimpleNode & terminal = terminal_set[i];
-//			assert( 0 == terminal.countNetDevices());
-//			topology_helper.InstallCsmaLink(terminal, csma_switch, 5000000, 2);
-//		}	// for
-//	}	// for
-
 	WifiStaNodeSets wifi_sta_node_sets(350);
-//	std::vector<ns3::Ptr<WifiStaNodeSet> > wifi_sta_node_sets;
-//	for (int i = 0; i < 350; ++i) {
-//		wifi_sta_node_sets.push_back(new WifiStaNodeSet);
-//	} //for
 	eunet.Attach(wifi_sta_node_sets);
-//	for (int i = 1; i <= 315; ++i) {
-//		if (i == 298 || i == 299 || i == 306 || i == 315)
-//			continue;
-//		eunet.Attach(*wifi_sta_node_sets[i], i);
-//		DefaultWifiPhyHelper* p_default_wifi_phy_helper =
-//				new DefaultWifiPhyHelper();
-//		wifi_sta_node_sets[i]->install(*p_default_wifi_phy_helper);
-//		wifi_ap_nodes[i]->install(*p_default_wifi_phy_helper);
-//		delete p_default_wifi_phy_helper;
-//	}	//for
-	//wifi area
-
-//	std::vector<DefaultMobilityHelper> mobilities(316);
-//	for (int i = 0; i <= 315; ++i) {
-//		//		if (i==0 || i == 298 || i == 299 || i == 306 || i == 315)
-//		//			continue;
-//		DefaultMobilityHelper default_mobility_helper;
-//		wifi_sta_node_sets[i]->install(default_mobility_helper);
-//	}
-
-// We've got the "hardware" in place.  Now we need to add IP addresses.
 
 	ns3::Ipv4AddressHelper ipv4;
 	ipv4.SetBase(ns3::Ipv4Address("133.71.0.0"), ns3::Ipv4Mask("255.255.0.0"));
