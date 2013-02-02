e4039b6175ca23592b1d769c54f7337302574e70
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2013-02-02 23:43:53 +0900

    switchs are represented in the class Eunet

---
+++ eunet-simulation/Eunet.cpp
@@ -0,0 +1,18 @@
+/*
+ * Eunet.cpp
+ *
+ *  Created on: Feb 2, 2013
+ *      Author: sasaki
+ */
+
+#include "Eunet.h"
+
+Eunet::Eunet() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Eunet::~Eunet() {
+	// TODO Auto-generated destructor stub
+}
+
